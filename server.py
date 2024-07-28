225ef4a01f3f773df13fd2d1a69e9e4255f072de
PieroAGG <alexailypiero@gmail.com>
2024-07-28 17:01:10 +0000

    Mundo guardado 2024-07-28

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 # Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
-# Puedes instalar mohist después de instalar forge desde el menú de gestionar
+# Puedes instalar mohist después de instalas forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
 
-# Regiones de ngrok
+# Regiones de ngroK
 # Código          Lugar
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
