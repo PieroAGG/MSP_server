fa7a6e9eb85bcc1327bc05a81a19ad6dcadb1140
PieroAGG <alexailypiero@gmail.com>
2024-08-02 23:30:37 +0000

    Mundo guardado 2024-08-02

--- server.py
+++ server.py
@@ -3,7 +3,8 @@
 
 # Puedes instalar mohist después de instalas forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
-# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
+# Puedes instalar purpu
+# r después de instalar fabric desde el menú de gestionar
 
 # Regiones de ngrok
 # Código          Lugar
